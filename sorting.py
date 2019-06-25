7e5d68978f8c17abf77a89b0595bbfbcdea8485c
buvimasi <41372984+buvimasi@users.noreply.github.com>
2019-06-25 15:37:20 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import numpy as np
 
-unsorted=pd.DataFrame(np.random.randn(10,2),index=[1,4,6,2,3,5,9,8,0,7],colu
+un_sorted=pd.DataFrame(np.random.randn(10,2),index=[1,4,6,2,3,5,9,8,0,7],colu
 mns=['col2','col1'])
 print(unsorted)
 #by label
-sorted=unsorted.sort_index()
+sorted=un_sorted.sort_index()
 print(sorted)
 #by axis=1
-sorted=unsorted.sort_index(axis=1)
+sorted=un_sorted.sort_index(axis=1)
 print(sorted)
 #sort descending order
-sorted=unsorted.sort_index(ascending=False)
+sorted=un_sorted.sort_index(ascending=False)
 #by value
-sorted=unsorted.sort_values(by='col1')
+sorted=un_sorted.sort_values(by='col1')
 #sorting algorithm
-sorted=unsorted.sort_values(by='col1',kind='mergesort')
+sorted=un_sorted.sort_values(by='col1',kind='mergesort')
